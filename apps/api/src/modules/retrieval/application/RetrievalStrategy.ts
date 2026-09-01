486e6752f443757ff701edfad8881708e603784c
Rudra Mahyavanshi <rudramahyavanshi15@gmail.com>
2026-09-01 19:38:20 +0530

    retrieval strategy

---
+++ apps/api/src/modules/retrieval/application/RetrievalStrategy.ts
@@ -0,0 +1,13 @@
+import type { RetrievedChunk } from "../infrastructure/PgVectorRepository.js";
+
+export type RetrievalInput = {
+  queryEmbedding: number[];
+  userId: string;
+  limit?: number;
+};
+
+export interface RetrievalStrategy {
+  execute(
+    input: RetrievalInput,
+  ): Promise<RetrievedChunk[]>;
+}
